42c58a4b46dc95bc7c23bf7dec5f3894c404732c
mikes <msaelens@gmail.com>
2013-12-22 16:15:15 -0500

    Add routes and separate pages for each meal. Add images.

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,33 @@
-'use strict';
-
 angular.module('mp2App', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('#about', {
-        templateUrl: 'views/breakfast.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        "using strict";
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/breakfast', {
+                templateUrl: 'views/breakfast.html',
+                controller: 'MainCtrl'
+            })
+            .when('/lunch', {
+                templateUrl: 'views/lunch.html',
+                controller: 'MainCtrl'
+            })
+            .when('/dinner', {
+                templateUrl: 'views/dinner.html',
+                controller: 'MainCtrl'
+            })
+            .when('/dessert', {
+                templateUrl: 'views/dessert.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,10 +1,8 @@
-'use strict';
-
 angular.module('mp2App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
